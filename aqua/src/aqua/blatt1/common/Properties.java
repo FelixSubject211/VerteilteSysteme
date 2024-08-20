f4a2940cbc3fc5c3e9e8aca1e922b0d0f1b5714f
FelixSubject211 <113304291+FelixSubject211@users.noreply.github.com>
2024-08-20 17:20:57 +0200

    BrokerTask

--- aqua/src/aqua/blatt1/common/Properties.java
+++ aqua/src/aqua/blatt1/common/Properties.java
@@ -4,4 +4,5 @@ public class Properties {
 	public final static String HOST = "localhost";
 	public final static int PORT = 4711;
 	public final static String BROKER_NAME = "AquaBroker";
+	public final static int THREAD_POOL_SIZE = 4;
 }
